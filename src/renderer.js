df6dccb19f06e9b6c1f85f0ac26cc3b1ea72a5bc
Sparrowill <email@willsparrow.co.uk>
2024-01-04 13:49:08 +0000

    Non class based interaction dialog done

--- src/renderer.js
+++ src/renderer.js
@@ -16,7 +16,20 @@
 // Event Listener for buttons
 // TODO get element by class, extract id, then do stuff based on that, rather than many listeners
 document.getElementById("test").addEventListener("click", function (){
-    showText("Text ");
+   // showText("Text ");
+   showInteract("Interaction Test With longe rtext","Agree byt longer");
+});
+
+//Event listener for cancel interaction button
+document.getElementById("cancel").addEventListener("click", function (){
+    console.log("Cancelled");
+    document.getElementById("dialogue-bg").style.display = "none"
+});
+
+//Event listener for agree interaction button
+document.getElementById("agree").addEventListener("click", function (){
+    console.log("Agreed");
+    document.getElementById("dialogue-bg").style.display = "none"
 });
 
 // Function to display text in the dialogue box
@@ -24,7 +37,7 @@ function showText(text){
     container = document.getElementById("dialogue-bg");
     // Set text value (auto expands and wraps)
     document.getElementById("dialogue-text").innerHTML = text;
-    // dialogue box in
+    // dialogue box appears
     container.style.display = "block";
     display_duration = 1500 + ( text.length * 30 );
     // Display the text for a time period, depending on the length of the text
@@ -32,4 +45,15 @@ function showText(text){
         container.style.display = "none";
         console.log("Done");
     },display_duration);
+}
+
+//Function to display an interactive text dialogue box
+function showInteract(question, text){
+    container = document.getElementById("dialogue-bg");
+    // Set text value (auto expands and wraps)
+    document.getElementById("dialogue-text").innerHTML = question;
+    // Set button value
+    document.getElementById("agree").innerHTML = text;
+    // dialogue box appears
+    container.style.display = "block";
 }
