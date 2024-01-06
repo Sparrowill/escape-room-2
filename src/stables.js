292d7fc19b770db2a09178db5c0c70e7a32d3da1
Sparrowill <email@willsparrow.co.uk>
2024-01-06 22:10:49 +0000

    minor changes

--- src/stables.js
+++ src/stables.js
@@ -10,7 +10,7 @@ export function create_stables(){
     stables_entrance.add_nav_btn("to-mirza","50","20",move,"stables-mirza");
     stables_entrance.add_feedback_btn("sleigh","40","60","40","20", "10",show_text,"You have clicked the sleigh");
     stables_entrance.add_feedback_btn("cart","60","40","40","20", "10",show_text,"You have clicked the cart");
-    stables_entrance.add_interact_btn("hassan","15","10","0","2","4",show_interact,"Shall we print 'Hassan' to console?", "Print",function(){console.log("Hassan");})
+    stables_entrance.add_interact_btn("hassan","10","14","0","3","4",show_interact,"Shall we print 'Hassan' to console?", "Print",function(){console.log("Hassan");})
     window.scenes.push(stables_entrance);
     
     stables_interior = new Scene("stables-interior","./images/backgrounds/stables/interior.png");
