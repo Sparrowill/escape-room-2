a9340fabc1fb423b6e603bb67127096542a034df
Sparrowill <email@willsparrow.co.uk>
2024-01-10 16:52:15 +0000

    typo

--- src/stables.js
+++ src/stables.js
@@ -64,7 +64,7 @@ export function create_stables(){
     stables_mirza.add_feedback_btn("mirza","22","32","0","5","8",show_text,"It's a name plaque for a horse. It says 'Mirza'.");
     stables_mirza.add_feedback_btn("lance","0","10","0","10","18",show_text,"That's a lance. Seeing as I don't have a squire, or a huge interest in jousting. I'm going to leave it here.");
     stables_mirza.add_feedback_btn("sink","35","50","0","10","10",show_text,"It's either a sink, or a feed bin. I don't know. Either way it's empty.");
-    stables_mirza.add_feedback_btn("pommel","32","10","0","30","18",show_text,"A pair of woderfully ornate pommels. Useful for topping things, I suppose.");
+    stables_mirza.add_feedback_btn("pommel","32","10","0","30","18",show_text,"A pair of wonderfully ornate pommels. Useful for topping things, I suppose.");
     stables_mirza.add_feedback_btn("case","45","10","0","12","24",show_text,"There's nothing of interest in this case");
 
     window.scenes.push(stables_mirza);
