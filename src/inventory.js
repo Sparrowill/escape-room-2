d7fcd5ae4d6675abc1c745b61bdf91fbfd2172c7
Sparrowill <email@willsparrow.co.uk>
2024-01-22 18:35:12 +0000

    added window clue

--- src/inventory.js
+++ src/inventory.js
@@ -5,7 +5,7 @@ export function create_inventory(){
     room.create_inventory_item("gun-key", "./images/inventory/gun_key.png");
     room.create_inventory_item("gun-clue", "./images/inventory/gun_clue.png");
     room.create_inventory_item("music-key", "./images/inventory/music-key.png");
-    room.create_inventory_item("window-clue", "./images/inventory/placeholder.png");
+    room.create_inventory_item("window-clue", "./images/inventory/window-clue.png");
     room.create_inventory_item("stables-bench-clue", "./images/inventory/billiard-clue.png");
     room.create_inventory_item("billiard-ball", "./images/inventory/billiard-ball.png");
 }
