4e0df271da54ec9563034194b361afd7df2efbff
Sparrowill <email@willsparrow.co.uk>
2024-02-18 13:41:09 +0000

    Rigged smoking entrance

--- src/smoking.js
+++ src/smoking.js
@@ -1,5 +1,5 @@
 import {Scene} from "./scene.js"
-import {move, show_interact,show_text} from "./interaction.js"
+import {move, show_interact, show_text} from "./interaction.js"
 import { smoking_chess, smoking_exit } from "./puzzles.js";
 
 export function create_smoking(){
@@ -18,11 +18,21 @@ export function create_smoking(){
     smoking_entrance.add_interact_btn("exit","59","15","0","37","5",show_interact,"This is the exit. Want to have a look?", "Yes", function(){
         smoking_exit.activate_door();
     })
+    smoking_entrance.add_interact_btn("exit","64","15","0","17","5",show_interact,"This is the exit. Want to have a look?", "Yes", function(){
+        smoking_exit.activate_door();
+    })
+    smoking_entrance.add_feedback_btn("large-painting","0","10","0","30","27",show_text,"A large painting. Disappointingly nothing behind it.");
+    smoking_entrance.add_feedback_btn("small-paintings","29","10","0","28","8",show_text,"Some smaller portraits of occupants of the house.");
+    smoking_entrance.add_feedback_btn("seating","15","40","0","40","35",show_text,"This being a smoking room, it seems apt to have deep burgundy furniture to recline on with a cigar and a port after a large lunch. Unfortunately you have neither of these things, so keep off");
+    smoking_entrance.add_feedback_btn("vase","82","40","0","30","9",show_text,"Just a big ol' vase.");
+
+
     window.scenes.push(smoking_entrance);
 
     var smoking_interior = new Scene("smoking-interior", "./images/backgrounds/smoking/interior.png");
     smoking_interior.add_nav_btn("to-billiards","37","30",move,"billiards-entrance");
     smoking_interior.add_nav_btn("to-entrance","45","50",move,"smoking-entrance");
+    
     smoking_interior.add_interact_btn("cabinet","58","38","0","10","5",show_interact,"A cabinet. Open it?","Yes", function(){
        
         if(smoking_chess.is_solved()){
